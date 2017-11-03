79351350563aa795a827b1d3b5d27311e7799ff3
datahasbyte <33130452+datahasbyte@users.noreply.github.com>
2017-11-03 06:11:47 -0400

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -2,44 +2,52 @@
 ## reads all files and combines like data frames
 ## finally, we tidy the data finding the mean of every column and export
 
+library(reshape2)
+library(dplyr)
 
-# setwd("E:\\Dropbox\\coursera\\Getting and Cleaning Data\\UCI HAR Dataset")
-setwd("~/Dropbox/coursera/Getting and Cleaning Data/UCI HAR Dataset")
+setwd("E:\\Dropbox\\coursera\\Getting and Cleaning Data\\UCI HAR Dataset")
 
-X_train <- read.table("./train/X_train.txt")
-y_train <- read.table("./train/y_train.txt")
-X_test <- read.table("./test/X_test.txt")
-y_test <- read.table("./test/y_test.txt")
-subject_test <- read.table("./test/y_test.txt")
+X_train <- read.table(".\\train\\X_train.txt")
+y_train <- read.table(".\\train\\y_train.txt")
+X_test <- read.table(".\\test\\X_test.txt")
+y_test <- read.table(".\\test\\y_test.txt")
+subject_test <- read.table(".\\test\\subject_test.txt")
+subject_train <- read.table(".\\train\\subject_train.txt")
+features <- read.table(".\\features.txt")[,2]
+activities <- read.table(".\\activity_labels.txt")[,2] # activity names 
 
-features <- read.table("./features.txt")[,2]
-# features1 <- read.table(".\\features.txt")
-activities <- read.table("./activity_labels.txt")[,2] # activity names 
-
-# str(features)
+# Linux
+# setwd("~/Dropbox/coursera/Getting and Cleaning Data/UCI HAR Dataset")
 # 
-# head(X_train)
-# head(y_train)
-# head(X_test)
-# head(y_test)
+# X_train <- read.table("./train/X_train.txt")
+# y_train <- read.table("./train/y_train.txt")
+# X_test <- read.table("./test/X_test.txt")
+# y_test <- read.table("./test/y_test.txt")
+# subject_test <- read.table("./test/y_test.txt")
+# features <- read.table("./features.txt")[,2]
+# activities <- read.table("./activity_labels.txt")[,2] # activity names 
 
-names(X_test) <- features # set the names of data frame
-names(X_train) <- features # set the names of data frame
+# name setting
+names(X_test) <- features
+names(X_train) <- features
+names(subject_df) <- "SubjectId"
 
 X_df <- rbind(X_test, X_train) # merge X frames
-y_df <- rbind(y_test, subject_test, y_train) # merge y frames
+y_df <- rbind(y_test, y_train) # merge y frames
+subject_df <- rbind(subject_test, subject_train) # merge subject frames
 
-X_df <- X_df[,grepl(".*(.*mean|.*std).*", features)] # filter columns with mean or std in name
+# filter columns with mean or std in name
+X_df <- X_df[,grepl(".*(.*mean|.*std).*", features)]
 
-activities
-rep(activities, X_df)
-
+# set names of activities based on activity value
 y_df[,1]
 y_df[,2] <- activities[y_df[,1]]
-names(y_df) <- c('ActivityId', 'Activity') # set names of activities based on activity value
+names(y_df) <- c('ActivityId', 'Activity')
 
+# merge data and subjects
+combined_df <- cbind(X_df, subject_df, y_df)
 
-tidy_df <- colMeans(X_df) # find the mean of every column in dataframe
-tidy_df
+# created tidy (long) data and save to file
+melted_df <- melt(combined_df, (id.vars=c("SubjectId", "Activity")))
+dcast(melted_df, SubjectId + Activity ~  variable, mean) %>% write.table(file = ".\\tidy_data.txt")
 
-write.table(tidy_df, "./tidy_df.txt", sep="\t", row.names = FALSE) # export tidy dataframe
